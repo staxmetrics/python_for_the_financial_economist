861e7bd20ca90acc96d50624f9c266d02d5f1eec
stax25 <johanstaxjakobsen@gmail.com>
2021-09-04 14:09:46 +0200

    fix source folder

--- docs/conf.py
+++ docs/conf.py
@@ -13,10 +13,10 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(r'..\..'))
+sys.path.insert(0, os.path.abspath(r'..'))
 
 # other directories to create documentation
-autoapi_dirs = [r'..\..\codelib']
+autoapi_dirs = [r'..\codelib']
 
 # -- Project information -----------------------------------------------------
 
