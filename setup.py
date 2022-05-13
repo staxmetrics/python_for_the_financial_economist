9d3d72b9e4030f3c9389be727c86e993d2b77c17
stax25 <johanstaxjakobsen@gmail.com>
2022-05-13 11:57:58 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="codelib",
+    version="0.1",
+    author="Andrew Carter",
+    author_email="andrewjcarter@gmail.com",
+    description=("Helper functions for the course"
+                 "'Python for the Financial Economist'")
+)
