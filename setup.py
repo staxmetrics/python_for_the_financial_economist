5886d7d42404dc062e0d4cb8fba97d6fd4bdf55f
stax25 <johanstaxjakobsen@gmail.com>
2022-05-13 12:35:17 +0200

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
                  "'Python for the Financial Economist'"),
     packages=find_packages(),
     classifiers=['Programming Language :: Python :: 3'],
-    python_requires='=>3.8',
+    python_requires='>=3.8',
     install_requires=['matplotlib==3.3.3', 'numpy==1.19.5', 'pandas==1.2.1', 'pandas-datareader==0.10.0',
                       'Quandl==3.6.1', 'scipy==1.6.1', 'seaborn==0.11.1']
 )
