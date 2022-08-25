d3e2617c5846313a837ca0a4879d1ed6ecb0231f
stax25 <johanstaxjakobsen@gmail.com>
2022-08-25 21:38:34 +0200

    Add historical probabilities problem to exercises_week_3.ipynb
    Work on exercises for week 4

--- codelib/statistics/regression.py
+++ codelib/statistics/regression.py
@@ -7,7 +7,7 @@ from typing import Union
 class LinearRegression:
 
     """
-    Class the implements linear regression allowing for probability weights
+    Class the implements linear regression allowing for scenario probabilities
     """
 
     def __init__(self, y: np.ndarray, x: np.ndarray, probs: Union[bool, None]):
